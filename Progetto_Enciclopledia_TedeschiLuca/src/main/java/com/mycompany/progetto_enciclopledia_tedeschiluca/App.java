892306b8621e69eb72792f226681bddef347acc5
Tedeschi_Luca <80003495+tedeschiluca@users.noreply.github.com>
2024-04-29 13:48:23 +0200

    Add files via upload
    Creazione dell'eccezione PlantException e Impostazione delle classi con prova di output nel main

---
+++ Progetto_Enciclopledia_TedeschiLuca/src/main/java/com/mycompany/progetto_enciclopledia_tedeschiluca/App.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progetto_enciclopledia_tedeschiluca;
+
+import Eccezioni.PlantException;
+
+/**
+ *
+ * @author lucat
+ */
+public class App {
+
+    public static void main(String[] args) throws PlantException{
+        
+       
+        
+        plants Pianta = new plants ("Fico", "Ficus carica", "Mediterraneo", "Albero da frutto", 5);
+        System.out.println(Pianta);
+        
+        
+    }
+}
