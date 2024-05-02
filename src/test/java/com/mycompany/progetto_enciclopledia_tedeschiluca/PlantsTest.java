6596d2c024092214c333ddbd429dae9b1c972010
Singh A. <4rshfn@gmail.com>
2024-05-02 09:32:51 +0200

    Aggiunta del menù con tutti i controlli necessari come inserimenti di input sbagliato, creazione della classe menù, implementazione dell'arrayList

---
+++ src/test/java/com/mycompany/progetto_enciclopledia_tedeschiluca/PlantsTest.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.progetto_enciclopledia_tedeschiluca;
+
+import Eccezioni.PlantException;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Studente
+ */
+public class PlantsTest {
+    
+    public PlantsTest() {
+        
+    }
+
+    @Test
+    public void testGetNome() 
+    {
+        
+    }
+
+    /**
+     * Test of getNomeScientifico method, of class plants.
+     */
+    @Test
+    public void testGetNomeScientifico() {
+    }
+
+    /**
+     * Test of getAreaDiCrescita method, of class plants.
+     */
+    @Test
+    public void testGetAreaDiCrescita() {
+    }
+
+    /**
+     * Test of getCaratteristicheGenerali method, of class plants.
+     */
+    @Test
+    public void testGetCaratteristicheGenerali() {
+    }
+
+    /**
+     * Test of anniPianta method, of class plants.
+     */
+    @Test
+    public void testAnniPianta() {
+    }
+
+    /**
+     * Test of setNome method, of class plants.
+     */
+    @Test
+    public void testSetNome() {
+    }
+
+    /**
+     * Test of setNomeScientifico method, of class plants.
+     */
+    @Test
+    public void testSetNomeScientifico() {
+    }
+
+    /**
+     * Test of setAreaDiCrescita method, of class plants.
+     */
+    @Test
+    public void testSetAreaDiCrescita() {
+    }
+
+    /**
+     * Test of setCaratteristicheGenerali method, of class plants.
+     */
+    @Test
+    public void testSetCaratteristicheGenerali() {
+    }
+
+    /**
+     * Test of setAnniPianta method, of class plants.
+     */
+    @Test
+    public void testSetAnniPianta() {
+    }
+
+    /**
+     * Test of toString method, of class plants.
+     */
+    @Test
+    public void testToString() {
+    }
+    
+}
